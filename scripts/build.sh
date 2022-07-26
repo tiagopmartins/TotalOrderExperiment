70a0bce076045ab1d1e7f4d168e27437c3a7b553
tiagopmartins <tiagopmartins17@gmail.com>
2022-07-26 17:28:18 +0000

    Fixed build script: running the server

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Getting the IP of the network interface (eth0, wlan0, wlan1, ...).
+# tserver network interface: wlx7c8bc10c3f1
 PRIVATE_IP=`ifconfig eth0 | grep 'inet' | grep -v inet6 | sed -e 's/^[ \t]*//' | cut -d' ' -f2`
 PUBLIC_IP=$PRIVATE_IP
+PORT=50001
 
 # Compile the latest version of the code on the branch we just check out.
 mkdir build && cd build && cmake .. && make -j2 && cd ..
@@ -16,4 +18,4 @@ done
 # Tailor the config file to have process specific information.
 echo -e "server:" >> conf/anna-config.yml
 echo -e "    public_ip: $PUBLIC_IP" >> conf/anna-config.yml
-./build/server/server localhost 50001
+./build/server/server $PUBLIC_IP $PORT
