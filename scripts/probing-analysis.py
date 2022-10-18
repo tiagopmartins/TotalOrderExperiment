e9b12e0838ea66927387b98066ab502f92a3eae6
tiagopmartins <tiagopmartins17@gmail.com>
2022-10-18 10:41:41 +0000

    fix: corrected x-axis of the plots

--- scripts/probing-analysis.py
+++ scripts/probing-analysis.py
@@ -1,6 +1,7 @@
 import sys
 import json
 import matplotlib.pyplot as plt
+import numpy as np
 
 def parse(probing_file):
     '''
@@ -23,12 +24,14 @@ if __name__ == "__main__":
     probing_file = sys.argv[1]
     results = parse(probing_file)
 
-    fig = plt.figure(figsize=(15, 6))
+    fig = plt.figure(figsize=(25, 6))
     
     ax = fig.add_subplot(111)
     ax.set_title('Stability of the GRID5000 network')
     ax.set_xlabel('Time (s)')
     ax.set_ylabel('Round Trip Time (ms)')
-    
+
     bp = ax.boxplot(results, showfliers=False, widths=0.4)
+    plt.xticks(np.round(np.linspace(0, len(results), num=25), 0), np.round(np.linspace(0, len(results), num=25), 0))
+    
     plt.savefig('probing.png')
